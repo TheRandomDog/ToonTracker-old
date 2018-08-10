6ebad1b9483ce1d71c09198858f70ab0689a286b
TheRandomDog <tommeyms@gmail.com>
2018-08-10 16:01:52 -0500

    music: Get a TuneTracker in a voice channel

--- modules/music.py
+++ modules/music.py
@@ -2,24 +2,47 @@ from .module import *
 from extra.tunetracker import TuneTracker
 
 class MusicManager(Module):
-	class SummonBotCMD(Command):
-		NAME = 'summonBot'
+    class SummonBotCMD(Command):
+        NAME = 'summonBot'
 
-		@classmethod
-		async def execute(cls, client, module, message, *args):
-			try:
-				token = module.available.pop()
-				config = Config.getModuleSetting('music', 'bots')[token]
-			except IndexError:
-				return 'No available TuneTrackers.'
+        @classmethod
+        async def execute(cls, client, module, message, *args):
+            try:
+                token = module.available.pop()
+                config = Config.getModuleSetting('music', 'bots')[token]
+            except IndexError:
+                return 'No available TuneTrackers.'
 
-			bot = TuneTracker()
-			client.loop.create_task(bot.start(token))
+            bot = TuneTracker()
+            client.loop.create_task(bot.start(token))
 
-	def __init__(self, client):
-		Module.__init__(self, client)
+    class PlayCMD(Command):
+        NAME = 'play'
 
-		self.available = list(Config.getModuleSetting('music', 'bots', {}).keys())
-		self.runningBots = {}
+        @classmethod
+        async def execute(cls, client, module, message, *args):
+            if not message.author.voice:
+                return module.createDiscordEmbed(subtitle=':music: Please join a voice channel and I\'ll send a TuneTracker your way!')
+
+            if message.author.voice.channel.id not in module.running_bots:
+                await message.channel.send('Spinning up your TuneTracker, it\'ll just be a jif...')
+                bot = await module.spawn_bot(message.author.voice.channel)
+                await bot.get_channel(message.author.voice.channel.id).connect()
+
+    def __init__(self, client):
+        Module.__init__(self, client)
+
+        self.available = list(Config.getModuleSetting('music', 'bots', {}).keys())
+        self.running_bots = {}
+
+    async def spawn_bot(self, channel):
+        token = self.available.pop()
+        config = Config.getModuleSetting('music', 'bots')[token]
+
+        bot = TuneTracker()
+        self.client.loop.create_task(bot.start(token))
+        await bot.wait_until_ready()
+        self.running_bots[channel.id] = bot
+        return bot
 
 module = MusicManager
