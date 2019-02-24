989dc2ed193af90bfc0645dae28240b96c94c6bd
TheRandomDog <tommeyms@gmail.com>
2019-02-24 14:31:49 -0600

    module: Fix an error when error handling

--- modules/module.py
+++ modules/module.py
@@ -237,6 +237,6 @@ class PermaMessage:
             print(msg)
             await self.module.client.send_message(self.module.client.botspam, msg)
         except (asyncio.TimeoutError, aiohttp.client_exceptions.ClientOSError):
-            msg = '**{}** tried to update the **{}** perma-message, but the async call errored / timed out.'.format(module.__class__.__name__, update[1])
+            msg = '**{}** tried to update the **{}** perma-message, but the async call errored / timed out.'.format(self.module.__class__.__name__, update[1])
             print(msg)
             await self.module.client.send_message(self.module.client.botspam, msg)
